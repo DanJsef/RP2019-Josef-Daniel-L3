bf74090f471f699b9adc0b8bbfa72eb03e879b31
DanJos <dan.jsef@gmail.com>
2019-05-28 13:46:04 +0200

    final changes

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '3306',
-            'database' => 'rp',
-            'username' => 'root',
-            'password' => '1234',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'rp'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', '1234'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 1. Run composer install
 2. Run npm install
 3. Create database
-4.Create .env file (copy .env.example)
-5.Grant permission to storage (if needed)
+4. Create/Edit .env file (copy .env.example)
+5. Edit directus/config/api.php
+5. Grant permission to storage (if needed)
